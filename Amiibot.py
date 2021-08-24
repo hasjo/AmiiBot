8fd632542fb126e782efe0348d19d21de2565c03
jozz024 <74272560+jozz024@users.noreply.github.com>
2021-08-24 05:33:39 -0500

    almost done reworking it to use discord commands

--- Amiibot.py
+++ Amiibot.py
@@ -2,13 +2,15 @@ import asyncio
 import logging
 import os
 import discord
+from discord.ext import commands
 import requests
 
 STORAGE_DIR = '.\amiibo'
 logging.basicConfig(format='%(levelname)s:%(message)s',
                     level=logging.INFO)
-
+PREFIX = '!'
 client = discord.Client()
+bot = commands.Bot(command_prefix=PREFIX, description=DESCRIPTION)
 
 def ingest_file(username, nickname):
     filesize = discord.Attachment.size
@@ -30,17 +32,9 @@ def ingest_file(username, nickname):
 
 
 
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    else:
-
-        cont = message.content.lower()
-
-        if cont.startswith('!help'):
-            logging.info("{} - {}".format(message.author, cont))
+@bot.command(name='help')
+async def help_cmd(ctx)
+            logging.info("{} - {}".format(ctx.author, ctx.message))
             retmsg = ("This is the AmiiBot help menu!\n"
                       "`!store <amiibo nickname>` (and attach the bin)\n"
                       "`!list`\n"
@@ -48,90 +42,96 @@ async def on_message(message):
                       "`!delete <amiibo nickname>`\n"
                       "`!send <Tag the host> <amiibo nickname>`\n"
                       "`!download <amiibonickname>`")
-            await message.channel.send(retmsg)
+            await ctx.send(retmsg)
 
-        if cont.startswith('!list'):
-            logging.info("{} - {}".format(message.author, cont))
+@bot.command(name='list')
+async def list_cmd(ctx)
+            logging.info("{} - {}".format(ctx.author, ctx.message))
             filelist = os.listdir(STORAGE_DIR)
-            amiibolist = [x for x in filelist if x.startswith(str(message.author.id))]
+            amiibolist = [x for x in filelist if x.startswith(str(ctx.author.id))]
             namelist = ['-'.join(x.split('-')[1:]).replace('.bin', '') for x in amiibolist]
             print(namelist)
             retmsg = "The amiibo you have stored are:\n" + '\n'.join(namelist)
-            await message.channel.send(retmsg)
+            await ctx.send(retmsg)
 
-        if cont.startswith('!delete'):
+@bot.command(name='delete')
+async def delete_cmd(ctx)
             try:
-                logging.info("{} - {}".format(message.author, cont))
-                splitlist = cont.split()
+                logging.info("{} - {}".format(ctx.author, ctx.message))
+                splitlist = ctx.message.split()
                 to_del = '_'.join(splitlist[1:])
-                filename = STORAGE_DIR + str(message.author.id) + '-' + to_del + '.bin'
+                filename = STORAGE_DIR + str(ctx.author.id) + '-' + to_del + '.bin'
                 os.remove(filename)
-                await message.channel.send('Successfully deleted {}'.format(to_del))
+                await ctx.send('Successfully deleted {}'.format(to_del))
             except Exception as exc:
                 logging.warning(exc)
-                await client.send_message(message.channel, 'Deletion Failed')
+                await ctx.send('Deletion Failed')
 
-        if cont.startswith('!rename'):
+@bot.command(name='rename')
+async def rename_cmd(ctx)
             try:
-                logging.info("{} - {}".format(message.author, cont))
-                arglist = cont.split()[1:]
+                logging.info("{} - {}".format(ctx.author, ctx.message))
+                arglist = ctx.message.split()[1:]
                 argstring = ' '.join(arglist)
                 oldname, newname = ['_'.join(x.strip().split()) for x in argstring.split(',')]
-                oldfilename = STORAGE_DIR + str(message.author.id) + '-' + oldname + '.bin'
-                newfilename = STORAGE_DIR + str(message.author.id) + '-' + newname + '.bin'
+                oldfilename = STORAGE_DIR + str(ctx.author.id) + '-' + oldname + '.bin'
+                newfilename = STORAGE_DIR + str(ctx.author.id) + '-' + newname + '.bin'
                 os.rename(oldfilename, newfilename)
-                await client.send_message(message.channel, 'Successfully renamed {} to {}'.format(oldname, newname))
+                await ctx.send('Successfully renamed {} to {}'.format(oldname, newname))
             except Exception as exc:
                 logging.warning(exc)
                 if 'No such file or directory' in str(exc):
-                    await client.send_message(message.channel, 'File does not exist, make sure your spelling is correct')
+                    await ctx.send('File does not exist, make sure your spelling is correct')
                 elif 'not enough values to unpack' in str(exc):
-                    await client.send_message(message.channel, 'Not enough names provided, should be comma separated')
+                    await ctx.send('Not enough names provided, should be comma separated')
                 elif 'too many values to unpack'in str(exc):
-                    await client.send_message(message.channel, 'Too many names provided, calm down')
+                    await ctx.send('Too many names provided, calm down')
                 else:
-                    await client.send_message(message.channel, 'Failed to rename')
+                    await ctx.send('Failed to rename')
 
-        if cont.startswith('!store'):
-            logging.info("{} - {}".format(message.author, cont))
-            if discord.Attachment.size != 540:      #and message.Attachment.url.endswith('bin')
-                nick = '_'.join(cont.split()[1:])
+@bot.command(name='store')
+async def store_cmd(ctx)
+            logging.info("{} - {}".format(ctx.author, ctx.message))
+            if ctx.message.attachment.size != 540:      #and message.Attachment.url.endswith('bin')
+                nick = '_'.join(ctx.message.split()[1:])
                 try:
-                    ingest_file(message.author, nick)
-                    await message.channel.send('Successfully stored - ' + nick)
+                    ingest_file(ctx.author, nick)
+                    await ctx.send('Successfully stored - ' + nick)
                 except Exception as exc:
                     logging.warning(exc)
-                    await message.channel.send('Failed to Store')
+                    await ctx.send('Failed to Store')
             else:
-                await message.channel.send('Improper bin size')
+                await ctx.send('Improper bin size')
 
 
-        if cont.startswith('!send'):
-            logging.info("{} - {}".format(str(message.author), cont))
-            splitlist = cont.split()
+@bot.command(name='send')
+async def send(ctx)
+            logging.info("{} - {}".format(ctx.author, ctx.message))
+            splitlist = ctx.message.split()
             if len(splitlist) < 3:
-                await client.send_message(message.channel, 'Improper send command, please format it like: `!send <@usertag> <amiibonickname>`')
+                await ctx.send('Improper usage of command, please format it as: `!send <@usertag> <amiibonickname>`')
             else:
                 try:
                     to_send = '_'.join(splitlist[2:])
-                    recipient = message.mentions[0]
-                    filename = STORAGE_DIR + str(message.author.id) + '-' + to_send + '.bin'
-                    sendname = str(message.author) + '-' + to_send + '.bin'
+                    recipient = ctx.message.mentions[0]
+                    filename = STORAGE_DIR + str(ctx.author.id) + '-' + to_send + '.bin'
+                    sendname = str(ctx.author) + '-' + to_send + '.bin'
                     await client.send_file(recipient, filename, filename=sendname)
                     await client.send_message(message.channel, 'Successfully sent {} to {}'.format(to_send, str(recipient)))
                 except Exception as exc:
                     logging.warning(exc)
                     if 'Cannot send messages to this user' in str(exc):
-                        await client.send_message(message.channel, 'Cannot send messages to this user, they may have DMs turned off')
+                        await ctx.send('Cannot send messages to this user, they may have DMs turned off')
                     elif 'No such file or directory' in str(exc):
-                        await client.send_message(message.channel, 'File does not exist, make sure your spelling is correct')
+                        await ctx.send('File does not exist, make sure your spelling is correct')
                     else:
-                        await client.send_message(message.channel, 'Failed to Send')
+                        await ctx.send('Failed to Send')
 
 
-        if cont.startswith('!download'):
-            logging.info("{} - {}".format(message.author, cont))
-            splitlist = cont.split()
+@bot.command(name='download')
+async def dl(ctx)
+            logging.info("{} - {}".format(message.author, ctx.message))
+            splitlist = ctx.message.split()
             amiiname = '_'.join(splitlist[1:])
             if len(splitlist) == 1:
                 await client.send_message(message.channel, 'No nickname defined, please try again')
